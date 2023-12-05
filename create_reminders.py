f9b5f8d900191ab0e53b5e92a1ada5d821c71b56
unknown <abhisheklfps@gmail.com>
2023-12-05 12:55:19 +0530

    voice-event

--- create_reminders.py
+++ create_reminders.py
@@ -1,24 +1,17 @@
 import datetime
 import os.path
-
+from datetime import timedelta
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 
-def main():
-  """Shows basic usage of the Google Calendar API.
-  Prints the start and name of the next 10 events on the user's calendar.
-  """
+def Create_Event(start_time, summary, ):
   creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
   if not creds or not creds.valid:
@@ -26,68 +19,35 @@ def main():
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file("../Api keys/google_calendar.json", SCOPES)
-      creds = flow.run_local_server(port=0)
+      creds = flow.run_local_server(port = 0)
     with open("token.json", "w") as token:
       token.write(creds.to_json())
 
   try:
-    service = build("calendar", "v3", credentials=creds)
-
+    service = build("calendar", "v3", credentials = creds)
+    end_time = start_time + timedelta(hours = 2)
     
-    # now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    # print("Getting the upcoming 10 events")
-    # events_result = (
-    #     service.events()
-    #     .list(
-    #         calendarId="primary",
-    #         timeMin=now,
-    #         maxResults=10,
-    #         singleEvents=True,
-    #         orderBy="startTime",
-    #     )
-    #     .execute()
-    # )
-    # events = events_result.get("items", [])
-
-    # if not events:
-    #   print("No upcoming events found.")
-    #   return
-
-    # # Prints the start and name of the next 10 events
-    # for event in events:
-    #   start = event["start"].get("dateTime", event["start"].get("date"))
-    #   print(start, event["summary"])
-
-
-
-    events = {
-      "summary" : "My birthday",
-      "location" : "NYC",
-      "description": "My bday through api call",
-      "colorId": 6,
-      "start":{
-        "dateTime": "2023-12-09T00:00:00+02:00",
-        "timeZone": "Asia/Kolkata"
+    event = {
+      'summary': f'{summary}',
+      'location': 'Bengaluru',
+      'description': '',
+      'start': {
+        'dateTime': start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+        'timeZone': 'Asia/Kolkata',
       },
-      "end":{
-        "dateTime": "2023-12-09T01:00:00+02:00",
-        "timeZone": "Asia/Kolkata"
+      'end': {
+        'dateTime': end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+        'timeZone': 'Asia/Kolkata',
       },
-      "recurrence":[
-        "RRULE: FREQ=DAILY; COUNT=1"
-      ],
-      "attendees":[
-        {"email": "abhishekmurthy364@gmail.com"}
-      ]
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
     }
-
-    event = service.events().insert(calendarId = "primary", body= events).execute()
-    print(f"event created {event.get('htmlLink')}")
-    
+    service.events().insert(calendarId = "primary", body = event).execute()
 
   except HttpError as error:
     print(f"An error occurred: {error}")
-
-
-if __name__ == "__main__":
-  main()
--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import speech_recognition as sr
+from datetime import datetime, timedelta
+from create_reminders import Create_Event
+
+def Add_Event(command_list):
+    try:
+        time_str, day_or_night, date_str, month_str = command_list[4:8] # split up everything to access one by one
+        summary = command_list[8:] #take the last words as the summary
+        
+        if 'th' in date_str or 'rd' in date_str: #format the date and extract the number 
+            date_str = date_str[0]
+        time_str = time_str.split(':') #split it into hours and minutes
+
+        start_time = datetime(
+            2023, 
+            datetime.strptime(month_str, "%B").month, #extracting the number of the month
+            int(date_str), 
+            int(time_str[0]),
+            int(time_str[1]), 
+            0
+        )
+
+        if day_or_night == 'p.m.':
+            start_time += timedelta(hours = 12) #adding 12 to the time for p.m.
+
+        Create_Event(start_time = start_time, summary = " ".join(summary))
+    except:
+        return "Error processing the command, please try again"
+
 
 listener = sr.Recognizer()
 try:
     with sr.Microphone() as source:
         print('Listening..')
-        voice = listener.listen(source = source)
-        command = listener.recognize_google(voice)
-        print(command)
-        if command.split(' ')[0] == 'Angela':
-            print(command)
+        voice = listener.listen(source = source) #Continuously listens to a voice
+        command = listener.recognize_google(voice) #speech to text
+        command_list = command.split(' ')
+        if command_list[0] == 'Angela':
+            if command_list[1:4] == ['add', 'an', 'event'] or command_list[1:4] == ['add', 'to', 'calendar']:
+                print(command)
+                Add_Event(command_list = command_list) 
 
 except:
     pass
